ea400e3cb8215e6d5cfbaa3ff11c76f1e025cb3e
nlirsz <nicolas.bsg@hotmail.com>
2025-12-08 14:41:55 -0300

    fix: convert build-on-render script to ESM

--- scripts/build-on-render.js
+++ scripts/build-on-render.js
@@ -1,4 +1,4 @@
-const { execSync } = require('child_process');
+import { execSync } from 'child_process';
 
 if (process.env.RENDER) {
     console.log('Detected Render environment. Running build...');
